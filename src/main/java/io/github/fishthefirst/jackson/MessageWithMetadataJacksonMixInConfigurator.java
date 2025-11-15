4a14a480af0a0775578af6a2a46f9b35400574ae
Emanuel Oliveira <emanuelfpoliveira@gmail.com>
2025-11-15 14:50:38 +0000

    Limit access

--- src/main/java/io/github/fishthefirst/jackson/MessageWithMetadataJacksonMixInConfigurator.java
+++ src/main/java/io/github/fishthefirst/jackson/MessageWithMetadataJacksonMixInConfigurator.java
@@ -1,6 +1,7 @@
 package io.github.fishthefirst.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.github.fishthefirst.data.MessageWithMetadata;
 
 import java.util.Objects;
@@ -8,9 +9,9 @@ import java.util.Objects;
 public final class MessageWithMetadataJacksonMixInConfigurator {
     public static void configureMixIn(ObjectMapper objectMapper) {
         Objects.requireNonNull(objectMapper, "ObjectMapper cannot be null");
+        objectMapper.registerModule(new JavaTimeModule());
         objectMapper.addMixIn(MessageWithMetadata.class, MessageWithMetadataMixin.class);
     }
 
-    private MessageWithMetadataJacksonMixInConfigurator() {
-    }
+    private MessageWithMetadataJacksonMixInConfigurator() {}
 }
--- src/main/java/io/github/fishthefirst/jackson/MessageWithMetadataMixin.java
+++ src/main/java/io/github/fishthefirst/jackson/MessageWithMetadataMixin.java
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import java.time.Instant;
 
-public abstract class MessageWithMetadataMixin {
+abstract class MessageWithMetadataMixin {
 
     @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "@class")
     public abstract Object getPayload();
 
     @JsonCreator
-    public MessageWithMetadataMixin(@JsonProperty("sourceDate") Instant sourceDate, @JsonProperty("payload") Object payload) {}
+    MessageWithMetadataMixin(@JsonProperty("sourceDate") Instant sourceDate, @JsonProperty("payload") Object payload) {}
 }
